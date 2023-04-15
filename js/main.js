2b83042cb4ff4107d4af1a44fa582bcbee218e51
MDCIrony <miguel.acastillodiaz@outlook.es>
2023-04-15 03:13:36 -0500

    Adding encrypt functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-import { alertMsg } from "./constants/textStates";
-
 // Get the important elements
 const inputText = document.getElementById("input-text");
 const outputText = document.getElementById("output-text");
@@ -9,19 +7,48 @@ const btnCopy = document.getElementById("btn-copy");
 const littleBoy = document.getElementById("little-boy");
 const alertText = document.getElementById("alert-text");
 
-function encryptText() {
-  outputText.innerHTML = inputText.value;
-}
+// All constants
+export const alertMsg = {
+  noText: "No messages found",
+  preparedText: "Click on Encrypt/Decrypt button and see the magic ;)",
+  postText: "You can copy your Encrypt/Decrypt text with copy button!",
+};
+
+const outputFirstMessage = "Enter the message you want to Encrypt/Decrypt";
+
+const encryptConditions = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
+
+// Onload event
+window.addEventListener("load", function () {
+  inputText.value = "";
+  outputText.innerHTML = outputFirstMessage;
+  alertText.innerHTML = alertMsg.noText;
+});
+
+// When you input text..
 
 function isSomeTextHere() {
   if (inputText.value !== "") {
     littleBoy.style.display = "none";
+    alertText.innerHTML = alertMsg.preparedText;
     return;
   }
   littleBoy.style.display = "inline-block";
 }
 
 function replyInputOnOutput() {
+  if (inputText.value === "") {
+    outputText.innerHTML = outputFirstMessage;
+    alertText.innerHTML = alertMsg.noText;
+    return;
+  }
+
   outputText.innerHTML = inputText.value;
 }
 
@@ -34,4 +61,22 @@ inputText.addEventListener(
   false
 );
 
-btnEncrypt.addEventListener("click", encryptText, false);
+// When you encrypt text...
+function encrypt() {
+  const initialText = inputText.value;
+
+  const encryptText = initialText.replace(/[aeiou]/g, (match) => {
+    return encryptConditions[match];
+  });
+
+  outputText.innerHTML = encryptText;
+}
+
+btnEncrypt.addEventListener("click", encrypt, false);
+/**
+ * 1. La letra "e" es convertida para "enter"
+ * 2. La letra "i" es convertida para "imes"
+ * 3. La letra "a" es convertida para "ai"
+ * 4. La letra "o" es convertida para "ober"
+ * 5. La letra "u" es convertida para "ufat"
+ */
